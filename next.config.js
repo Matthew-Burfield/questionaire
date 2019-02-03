b5da040bb1dd0f37494c03ea3a354d6db8cc3fcb
Matthew-Burfield <m1.burfield@gmail.com>
2019-02-04 09:42:53 +1000

    Updated to use latest now serverless architecture

--- next.config.js
+++ next.config.js
@@ -1,18 +1,36 @@
 // next.config.js
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === "development"
-    ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION
-    ? require("next/constants") // Get values from `next` package when building locally
-    : require("next-server/constants"); // Get values from `next-server` package when building on now v2
+const DotEnv = require("dotenv-webpack");
+const withTypescript = require("@zeit/next-typescript");
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config used to run in production.
-    return {};
+const config = {
+  target: "serverless",
+  webpack: config => {
+    config.plugins.push(new DotEnv());
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto"
+    });
+    return config;
   }
-
-  const withTypescript = require("@zeit/next-typescript");
-
-  return withTypescript();
 };
+
+module.exports = withTypescript(config);
+
+// const { PHASE_PRODUCTION_SERVER } =
+//   process.env.NODE_ENV === "development"
+//     ? {} // We're never in "production server" phase when in development mode
+//     : !process.env.NOW_REGION
+//     ? require("next/constants") // Get values from `next` package when building locally
+//     : require("next-server/constants"); // Get values from `next-server` package when building on now v2
+
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_PRODUCTION_SERVER) {
+//     // Config used to run in production.
+//     return {
+//       // target: "serverless"
+//     };
+//   }
+
+//   return withTypescript();
+// };
