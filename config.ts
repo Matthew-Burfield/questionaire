4d1a3d22e4d1ed24d98cdb15c7e014c4031dc8dd
Matthew-Burfield <m1.burfield@gmail.com>
2019-02-05 08:42:55 +1000

    Updated config to point to production server if in production mode

--- config.ts
+++ config.ts
@@ -1,4 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4000`;
+export const endpoint =
+  process.env.NODE_ENV === "production"
+    ? "https://burfield-questionaire-surver.now.sh"
+    : "http://localhost:4000";
 // export const perPage = 5;
 // export const tabTitle = "Sick fits!";
