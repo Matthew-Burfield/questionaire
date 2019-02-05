7524dc0b7e54dfd26517ec36ca4afc31cc4971a7
Matthew-Burfield <m1.burfield@gmail.com>
2019-02-06 07:36:35 +1000

    Updated api endpoint

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint =
   process.env.NODE_ENV === "production"
-    ? "https://burfield-questionaire-surver.now.sh"
+    ? "https://burfield-questionaire-server.now.sh/"
     : "http://localhost:4000";
 // export const perPage = 5;
 // export const tabTitle = "Sick fits!";
